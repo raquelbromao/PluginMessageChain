3a476b2109676b5802255dbfd176b00be0b7c459
raquelbromao <raquelbromao@gmail.com>
2016-08-25 15:33:04 -0300

    Primeiro commit oficial

---
+++ plugin.raquel.examples.helloworld/src/plugin/raquel/examples/helloworld/HelloWorldAction.java
@@ -0,0 +1,257 @@
+package plugin.raquel.examples.helloworld;
+
+//import plugin.raquel.examples.helloworld.ExpressionInvoke;
+import plugin.raquel.examples.helloworld.Results;
+
+//import java.util.regex.Pattern;
+
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+
+//import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+//import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+/*import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ExpressionStatement;*/
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+//import org.eclipse.swt.widgets.Text;
+
+/**
+ * HelloWorldAction is a simple example of using an action set to extend the
+ * Eclipse Workbench with a menu and toolbar action that prints the "Hello
+ * World" message.
+ */
+public class HelloWorldAction implements IWorkbenchWindowActionDelegate {
+	IWorkbenchWindow activeWindow = null;
+	public Shell shlMessageChain;
+	//private static Text results;
+	IProject projectSelection;
+	IPackageFragment[] packagesSelection;
+
+	/**
+	 * Lista os projetos da Workspace em utilização
+	 */
+	public IProject[] getAllProjects() {
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		return projects;
+	}
+
+	/*public static void splitMessageChain(String s) {
+		// retira o ";" do final da string
+		s = s.replace(";", " ");
+
+		// Quebra a variável quando acha . e armazena a sobra numa posição do
+		// array aux
+		// a().b() -> . é descartando e a() fica em aux[0] e b() em aux[1]
+		String[] aux = s.split(Pattern.quote("."));
+
+		// Pega o tamanho da string aux
+		// Imprime a variável aux na tela
+		results.append("Objeto: " + aux[0] + "\n");
+		for (int i = 1; i < aux.length; i++) {
+			results.append("Método[" + i + "]: " + aux[i] + "\n");
+		}
+
+		results.append("_______________________________________________________\n");
+	}*/
+
+	/*public static void verificaMessageChain(String s) {
+		// verifica se a expressão coletada é igual ao regex criado
+		// não foi usado [;] no final do regex pq o compilador nem lê se não
+		// houver ele no final
+		if (s.matches("[\\w]+([\\.]+[\\w]+[(]+[)]){2,}")) { // "[\\w]+([\\.]+[\\w]+[(]+[?\\w]+[)]){2,}")
+			results.append("\nMessage Chain: " + s + "\n");
+			splitMessageChain(s);
+		} else {
+			results.append("\nNão é Message Chain: " + s + "\n_______________________________________________________\n");
+		}
+	}*/
+
+	/*private void analyseClass(ICompilationUnit classe) throws JavaModelException {
+		// ICompilationUnit unit = classe;
+		// now create the AST for the ICompilationUnits
+		CompilationUnit parse = parse(classe);
+		ExpressionInvoke visitor = new ExpressionInvoke();
+		parse.accept(visitor);
+
+		// Imprime na tela o nome do método e o tipo de retorno
+		for (ExpressionStatement method : visitor.getMethods()) {
+			String t = null;
+			t = method.getExpression().toString();
+
+			verificaMessageChain(t);
+		}
+	}*/
+
+	/**
+	 * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+	 * Java source file
+	 *
+	 * @param unit
+	 * @return
+	 */
+	/*private static CompilationUnit parse(ICompilationUnit unit) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		return (CompilationUnit) parser.createAST(null); // parse
+	}*/
+
+	/**
+	 * Run the action. Display the plugin
+	 */
+	public void run(IAction proxyAction) {
+		// proxyAction has UI information from manifest file (ignored)
+		shlMessageChain = new Shell();
+		shlMessageChain.setSize(547, 300);
+		shlMessageChain.setText("Message Chain Plugin");
+		shlMessageChain.setLayout(null);
+
+		Label lbl = new Label(shlMessageChain, SWT.NONE);
+		lbl.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
+		lbl.setBounds(25, 10, 394, 15);
+		lbl.setText("Message Chain: all methods in workspace!");
+
+		Combo comboProjects = new Combo(shlMessageChain, SWT.NONE);
+		comboProjects.setBounds(25, 27, 425, 23);
+
+		// Gets all projects from workspace
+		IProject[] projects = getAllProjects();
+		for (int i = 0; i < projects.length; i++) {
+			comboProjects.add(projects[i].getName());
+		}
+
+		comboProjects.select(0);
+
+		Button btnApplyProjects = new Button(shlMessageChain, SWT.NONE);
+		btnApplyProjects.setSelection(true);
+		btnApplyProjects.setBounds(456, 25, 75, 25);
+		btnApplyProjects.setText("Apply");
+
+		Button btnCancel = new Button(shlMessageChain, SWT.NONE);
+		btnCancel.setBounds(456, 50, 75, 25);
+		btnCancel.setText("Cancel");
+
+		/*Button btnClear = new Button(shlMessageChain, SWT.NONE);
+		btnClear.setBounds(456, 124, 75, 25);
+		btnClear.setText("Clear");*/
+		
+		/*Button btnTeste = new Button(shlMessageChain, SWT.NONE);
+		btnTeste.setText("Teste");
+		btnTeste.setBounds(456, 155, 75, 25);*/
+
+		btnApplyProjects.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				try {
+					// remove todas as classes do projeto escolhido anteriormente
+					//comboClasses.removeAll();
+					// LIMPA A JANELA DOS RESULTADOS QUANDO SELECIONADO UM NOVO PROJETO
+					//results.setText("");
+
+					// Acha a raiz da workspace para criar/carregar o IProject selecionado pelo usuário
+					String nameProject = comboProjects.getItem(comboProjects.getSelectionIndex());
+					IWorkspace workspace = ResourcesPlugin.getWorkspace();
+					IWorkspaceRoot root = workspace.getRoot();
+
+					// Pega a raiz do projeto selecionado pelo usuário
+					projectSelection = root.getProject(nameProject);
+					//results.append("## NAME OF PROJECT: " + projectSelection.getName() + "\n");
+					//results.append("## PATH OF PROJECT: " + projectSelection.getFullPath() + "\n");
+					projectSelection.open(null);
+
+					// Gera a lista de todas as classes do projeto selecionado
+					// com o tipo IPackageFragment que obtenho todas as classes de um projeto
+					// IProject -> IPackageFragment -> ICompilationUnit -> arq.java
+					packagesSelection = JavaCore.create(projectSelection).getPackageFragments();
+
+					Results.main(null,packagesSelection);
+					//TesteDesign.main(null, packagesSelection);
+				} catch (CoreException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		/*btnApplyClass.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				try {
+					// LIMPA A JANELA DOS RESULTADOS QUANDO SELECIONADO UMA NOVA CLASSE
+					results.setText("");
+					String nameClass = comboClasses.getItem(comboClasses.getSelectionIndex());
+
+					for (IPackageFragment mypackage : packagesSelection) {
+						for (final ICompilationUnit compilationUnit : mypackage.getCompilationUnits()) {
+							String aux = compilationUnit.getElementName();
+							if (aux.equals(nameClass)) {
+								analyseClass(compilationUnit);
+							}
+						}
+					}
+
+				} catch (JavaModelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});*/
+
+		btnCancel.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				shlMessageChain.close();
+			}
+		});
+
+		shlMessageChain.pack();
+		shlMessageChain.open();
+		/*btnClear.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				results.setText("");
+			}
+		});*/
+		
+		/*btnTeste.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {							      
+				Results.main(null,packagesSelection);
+			}
+		});*/
+	}
+
+	// IActionDelegate method
+	public void selectionChanged(IAction proxyAction, ISelection selection) {
+		// do nothing, action is not dependent on the selection
+	}
+
+	// IWorkbenchWindowActionDelegate method
+	public void init(IWorkbenchWindow window) {
+		activeWindow = window;
+	}
+
+	// IWorkbenchWindowActionDelegate method
+	public void dispose() {
+		// nothing to do
+	}
+}
---
+++ plugin.raquel.examples.helloworld/src/plugin/raquel/examples/helloworld/TesteDesign.java
@@ -0,0 +1,221 @@
+package plugin.raquel.examples.helloworld;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import java.util.regex.Pattern;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
+//import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class TesteDesign {
+
+	protected Shell shell;	
+	private static Text results;
+	static IPackageFragment[] packagesSelection;
+	static Results window;
+
+	public static void Inicializa(IPackageFragment[] p) {
+		packagesSelection = p;
+	}
+	
+	public static void splitMessageChain(String s) {
+		// retira o ";" do final da string
+		s = s.replace(";", " ");
+
+		// Quebra a variável quando acha . e armazena a sobra numa posição do
+		// array aux
+		// a().b() -> . é descartando e a() fica em aux[0] e b() em aux[1]
+		String[] aux = s.split(Pattern.quote("."));
+
+		// Pega o tamanho da string aux
+		// Imprime a variável aux na tela
+		results.append("Objeto: " + aux[0] + "\n");
+		for (int i = 1; i < aux.length; i++) {
+			results.append("Método[" + i + "]: " + aux[i] + "\n");
+		}
+
+		results.append("_______________________________________________________\n");
+	}
+
+	public static void verificaMessageChain(String s) {
+		// verifica se a expressão coletada é igual ao regex criado
+		// não foi usado [;] no final do regex pq o compilador nem lê se não
+		// houver ele no final
+		if (s.matches("[\\w]+([\\.]+[\\w]+[(]+[)]){2,}")) { // "[\\w]+([\\.]+[\\w]+[(]+[?\\w]+[)]){2,}")
+			results.append("\nMessage Chain: " + s + "\n");
+			splitMessageChain(s);
+		} else {
+			results.append("\nNão é Message Chain: " + s + "\n_______________________________________________________\n");
+		}
+	}
+
+	private void analyseClass(ICompilationUnit classe) throws JavaModelException {
+		// ICompilationUnit unit = classe;
+		// now create the AST for the ICompilationUnits
+		CompilationUnit parse = parse(classe);
+		ExpressionInvoke visitor = new ExpressionInvoke();
+		parse.accept(visitor);
+
+		// Imprime na tela o nome do método e o tipo de retorno
+		for (ExpressionStatement method : visitor.getMethods()) {
+			String t = null;
+			t = method.getExpression().toString();
+
+			verificaMessageChain(t);
+		}
+	}
+
+	/**
+	 * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+	 * Java source file
+	 *
+	 * @param unit
+	 * @return
+	 */
+	private static CompilationUnit parse(ICompilationUnit unit) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		return (CompilationUnit) parser.createAST(null); // parse
+	}
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args, IPackageFragment[] p) {
+		try {
+			TesteDesign window = new TesteDesign();
+			window.open(p);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open(IPackageFragment[] p) {
+		Inicializa(p);
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(600, 600);
+		shell.setText("SWT Application");
+		shell.setLayout(null);
+		
+		Combo comboClasses = new Combo(shell, SWT.NONE);
+		comboClasses.setBounds(107, 7, 387, 23);
+
+		// Gera a lista de todas as classes do projeto selecionado
+		// com o tipo IPackageFragment que obtenho todas as classes de um
+		// projeto
+		// IProject -> IPackageFragment -> ICompilationUnit -> arq.java
+		try {
+			for (IPackageFragment mypackage : packagesSelection) {
+				for (final ICompilationUnit compilationUnit : mypackage.getCompilationUnits()) {
+					comboClasses.add(compilationUnit.getElementName());
+					// results.append("## PACKAGE NAME: " +
+					// mypackage.getElementName() + "\n");
+					// results.append("## [CLASSE] COMPILATION UNIT NAME: " +
+					// compilationUnit.getElementName() + "\n");
+					// classSelection = compilationUnit;
+					// analyseClass(compilationUnit);
+				}
+			}
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// deixa a primeira classe encontrada visível por default no combo
+		comboClasses.select(0);
+
+		Label lbl = new Label(shell, SWT.NONE);
+		lbl.setBounds(10, 10, 91, 15);
+		lbl.setText("Select the class:");
+
+		Button btnApplyClass = new Button(shell, SWT.NONE);
+		btnApplyClass.setBounds(500, 7, 75, 25);
+		btnApplyClass.setText("Apply");
+
+		results = new Text(shell, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+		results.setBounds(10, 49, 484, 334);
+
+		Button btnReturn = new Button(shell, SWT.NONE);
+		btnReturn.setText("Return");
+		btnReturn.setBounds(500, 80, 75, 25);
+
+		Button btnClear = new Button(shell, SWT.NONE);
+		btnClear.setText("Clear");
+		btnClear.setBounds(500, 49, 75, 25);
+		
+		btnApplyClass.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				try {
+					// LIMPA A JANELA DOS RESULTADOS QUANDO SELECIONADO UMA NOVA CLASSE
+					results.setText("");
+					String nameClass = comboClasses.getItem(comboClasses.getSelectionIndex());
+
+					for (IPackageFragment mypackage : packagesSelection) {
+						for (final ICompilationUnit compilationUnit : mypackage.getCompilationUnits()) {
+							String aux = compilationUnit.getElementName();
+							if (aux.equals(nameClass)) {
+								analyseClass(compilationUnit);
+								
+							}
+						}
+					}
+
+				} catch (JavaModelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		btnReturn.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				shell.close();
+			}
+		});
+
+		btnClear.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				results.setText("");
+			}
+		});
+
+		
+	}
+}
